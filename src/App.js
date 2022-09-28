24bf9d4cf33f0505c374e6b95d6bf0915c10a201
nikunjkumarparmar <parmar.nikunjkumar@gmail.com>
2022-09-28 18:55:19 +0200

    auth component with router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Button, Container,Form,FormLabel,Jumbotron } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Auth from './auth/Auth';
 
-function App() {
-  return (
-    <div>
-      Welcome to my first React App.
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Auth/>}></Route>
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App;
